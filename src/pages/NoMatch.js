99510149e4fd5cd1e4648704afa4a6d02511ccdb
Tanja <tanja120a@gmail.com>
2022-11-12 14:48:42 +0100

    Create NoMatch page

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,6 +1,17 @@
+//components
+import  CommentingLoader  from '../components/loaders/CommentingLoader';
+//styles
+import { LoadingStyles } from '../components/loaders/LoadingStyles';
+
 const NoMatch = () => {
     return(
-        <div>No match found</div>
+        <LoadingStyles>
+          <CommentingLoader/>
+          <div>
+            <h2>oops!</h2>
+            <p>the page doesn't seem to exist </p>
+          </div>
+        </LoadingStyles>
     )
 }
 
